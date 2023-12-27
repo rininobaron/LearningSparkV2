5a6184056337818ec5c6fc8c55aaf68b158ef95b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-12-27 02:15:59 -0600

    Add first example script in scala with Datafrme definition

---
+++ chapter3/scala/script-examples/example1.scala
@@ -0,0 +1,14 @@
+// In Scala
+import org.apache.spark.sql.functions.avg
+import org.apache.spark.sql.SparkSession
+// Create a DataFrame using SparkSession
+val spark = SparkSession
+.builder
+.appName("AuthorsAges")
+.getOrCreate()
+// Create a DataFrame of names and ages
+val dataDF = spark.createDataFrame(Seq(("Brooke", 20), ("Brooke", 25),
+	("Denny", 31), ("Jules", 30), ("TD", 35))).toDF("name", "age")
+// Group the same names together, aggregate their ages, and compute an average
+val avgDF = dataDF.groupBy("name").agg(avg("age"))
+// Show the results of the final execution
