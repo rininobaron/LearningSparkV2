ae6c8f70ad7075046125810fded00fc4fb933f5b
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-12-27 19:55:34 -0600

    Change crateDataFrame method by sparkContext.parallelize

--- chapter3/scala/script-examples/example1.scala
+++ chapter3/scala/script-examples/example1.scala
@@ -1,14 +1,16 @@
 // In Scala
 import org.apache.spark.sql.functions.avg
 import org.apache.spark.sql.SparkSession
+
 // Create a DataFrame using SparkSession
-val spark = SparkSession
-.builder
-.appName("AuthorsAges")
-.getOrCreate()
+val spark = SparkSession.builder.appName("AuthorsAges").getOrCreate()
+
 // Create a DataFrame of names and ages
-val dataDF = spark.createDataFrame(Seq(("Brooke", 20), ("Brooke", 25),
+val dataDF = spark.sparkContext.parallelize(Seq(("Brooke", 20), ("Brooke", 25),
 	("Denny", 31), ("Jules", 30), ("TD", 35))).toDF("name", "age")
+
 // Group the same names together, aggregate their ages, and compute an average
 val avgDF = dataDF.groupBy("name").agg(avg("age"))
+
 // Show the results of the final execution
+avgDF.show()
