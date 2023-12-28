d2fafcbca5374940b80137c5aa9f2092d95e46b1
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-12-28 00:41:29 -0600

    Add python script with example fo define an scema file, read csv file 'fire department calls' and make some tanformations

---
+++ chapter3/py/src/example5.py
@@ -0,0 +1,67 @@
+# In Python, define a schema
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# Create a DataFrame using SparkSession
+spark = (SparkSession
+	.builder
+	.appName("example5")
+	.getOrCreate())
+
+# Programmatic way to define a schema
+fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
+	StructField('UnitID', StringType(), True),
+	StructField('IncidentNumber', IntegerType(), True),
+	StructField('CallType', StringType(), True),
+	StructField('CallDate', StringType(), True),
+	StructField('WatchDate', StringType(), True),
+	StructField('CallFinalDisposition', StringType(), True),
+	StructField('AvailableDtTm', StringType(), True),
+	StructField('Address', StringType(), True),
+	StructField('City', StringType(), True),
+	StructField('Zipcode', IntegerType(), True),
+	StructField('Battalion', StringType(), True),
+	StructField('StationArea', StringType(), True),
+	StructField('Box', StringType(), True),
+	StructField('OriginalPriority', StringType(), True),
+	StructField('Priority', StringType(), True),
+	StructField('FinalPriority', IntegerType(), True),
+	StructField('ALSUnit', BooleanType(), True),
+	StructField('CallTypeGroup', StringType(), True),
+	StructField('NumAlarms', IntegerType(), True),
+	StructField('UnitType', StringType(), True),
+	StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+	StructField('FirePreventionDistrict', StringType(), True),
+	StructField('SupervisorDistrict', StringType(), True),
+	StructField('Neighborhood', StringType(), True),
+	StructField('Location', StringType(), True),
+	StructField('RowID', StringType(), True),
+	StructField('Delay', FloatType(), True)])
+
+# Use the DataFrameReader interface to read a CSV file
+sf_fire_file = "../../../databricks-datasets/learning-spark-v2/sf-fire/sf-fire-calls.csv"
+fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
+#fire_df.show(5)
+
+# Return the rows where "CallType" is different from "Medical Incident"
+few_fire_df = (fire_df
+	.select("IncidentNumber", "AvailableDtTm", "CallType")
+	.where(col("CallType") != "Medical Incident"))
+few_fire_df.show(5, truncate=False)
+
+# In Python, return number of distinct types of calls using countDistinct()
+from pyspark.sql.functions import *
+(fire_df
+	.select("CallType")
+	.where(col("CallType").isNotNull())
+	.agg(countDistinct("CallType").alias("DistinctCallTypes"))
+	.show())
+
+# Filter for only distinct non-null CallTypes from all the rows
+(fire_df
+	.select("CallType")
+	.where(col("CallType").isNotNull())
+	.distinct()
+	.show(10, False))
